f0b8ed4017bf0648f23d76a3b5ba8d1b73a287b9
DilshanGDT <tharakadilshan506@gmail.com>
2025-04-14 20:27:24 +0530

    main routes and app.tsx

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import AppRoutes from './routes';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-[#faf6f1]">
+        <Navbar />
+        <main className="pt-16">
+          <AppRoutes />
+        </main>
+        {/* <Footer /> */}
+      </div>
+    </Router>
+  );
+}
+
+export default App;
